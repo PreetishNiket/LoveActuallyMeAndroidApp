addaf57a07325576544e1a29e4baec769e758ece
Preetish Niket <niketpreetish@gmail.com>
2020-08-11 23:36:43 +0530

    Added ProfileInfoActivity.kt

---
+++ app/src/main/java/com/example/loveactuallymeandroidapp/ui/ProfileInfo2Activity.kt
@@ -0,0 +1,12 @@
+package com.example.loveactuallymeandroidapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.loveactuallymeandroidapp.R
+
+class ProfileInfo2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_info2)
+    }
+}
--- app/src/main/java/com/example/loveactuallymeandroidapp/ui/ProfileInfoActivity.kt
+++ app/src/main/java/com/example/loveactuallymeandroidapp/ui/ProfileInfoActivity.kt
@@ -1,13 +1,21 @@
 package com.example.loveactuallymeandroidapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.loveactuallymeandroidapp.R
+import kotlinx.android.synthetic.main.activity_profile_info.*
 
 class ProfileInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_info)
         supportActionBar?.hide()
+        set_pro.setOnClickListener {
+            startActivity(Intent(this,ProfileInfo2Activity::class.java))
+        }
+        back1.setOnClickListener {
+            finish()
+        }
     }
 }
