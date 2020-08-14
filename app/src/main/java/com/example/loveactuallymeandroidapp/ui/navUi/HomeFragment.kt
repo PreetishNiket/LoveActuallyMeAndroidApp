a3527270db48aac8b30361708263e923d35d9479
Preetish Niket <niketpreetish@gmail.com>
2020-08-15 02:03:17 +0530

    Fixed Bug in BottomSheet

--- app/src/main/java/com/example/loveactuallymeandroidapp/ui/navUi/HomeFragment.kt
+++ app/src/main/java/com/example/loveactuallymeandroidapp/ui/navUi/HomeFragment.kt
@@ -3,36 +3,28 @@ package com.example.loveactuallymeandroidapp.ui.navUi
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 
-import com.example.loveactuallymeandroidapp.BottomSheetFragment
 import com.example.loveactuallymeandroidapp.R
 import com.example.loveactuallymeandroidapp.ui.LikeYouActivity
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import kotlinx.android.synthetic.main.bottom_sheet_layout.*
 import kotlinx.android.synthetic.main.bottom_sheet_layout.view.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class HomeFragment : Fragment(), View.OnClickListener {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -56,29 +48,9 @@ class HomeFragment : Fragment(), View.OnClickListener {
         likeButton.setOnClickListener {
             startActivity(Intent(context, LikeYouActivity::class.java))
         }
+
         return frag
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-
     override fun onClick(view: View) {
         if (view.id==R.id.button){
             view.setBackgroundColor(resources.getColor(R.color.pink))
---
+++ app/src/main/java/com/example/loveactuallymeandroidapp/utlis/Profile.kt
@@ -0,0 +1,9 @@
+package com.example.loveactuallymeandroidapp.utlis
+
+data class Profile(
+    val age: Int,
+    val distance: Int,
+    val id: Int,
+    val name: String,
+    val profile_pic: String
+)
