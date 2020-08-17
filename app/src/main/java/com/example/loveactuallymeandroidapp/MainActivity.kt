cef8f3d2689de765b244de712a34da2c72a08fbb
kishoretheeraj <kishoretheeraj@gmail.com>
2020-08-17 16:00:15 +0530

    first commit

--- app/src/main/java/com/example/loveactuallymeandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/loveactuallymeandroidapp/MainActivity.kt
@@ -14,6 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     private val homeFragment=HomeFragment()
     private val chatFragment=ChatFragment()
+    //firebasebranch
     private val mBottomNav=BottomNavigationView.OnNavigationItemSelectedListener {
         when(it.itemId){
             R.id.home_menu ->{
