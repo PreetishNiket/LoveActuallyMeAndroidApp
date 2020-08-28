f03dc857bcaf99e75bfaa03e71e492c9f150c4f7
Preetish Niket <niketpreetish@gmail.com>
2020-08-29 00:11:22 +0530

    Added Phone Number Verification

--- app/src/main/java/com/example/loveactuallymeandroidapp/ui/auth/OtpActivity.kt
+++ app/src/main/java/com/example/loveactuallymeandroidapp/ui/auth/OtpActivity.kt
@@ -3,21 +3,81 @@ package com.example.loveactuallymeandroidapp.ui.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.loveactuallymeandroidapp.R
 import com.example.loveactuallymeandroidapp.ui.profile.SocializingActivity
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
 import kotlinx.android.synthetic.main.activity_otp.*
+import java.util.concurrent.TimeUnit
 
 class OtpActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
         supportActionBar?.hide()
-        verify_button.setOnClickListener {
-            startActivity(Intent(this,SocializingActivity::class.java))
-        }
+
+        val phoneNumber = intent.getStringExtra("phoneNo")
+
         back_finish.setOnClickListener {
             finish()
         }
-        pinView.setTextColor(resources.getColor(R.color.white))
+        if (phoneNumber != null) {
+            sendCode(phoneNumber)
+        }
+        verify_button.setOnClickListener {
+            val code=pinView.text.toString()
+            if (code.isNotEmpty()){
+                verifyCode(code)
+            }
+        }
+    }
+    //global
+    private lateinit var storedVerificationId:String
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
+    private val callbacks=object :PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            val code= credential.smsCode
+            if (code!=null){
+                pinView.setText(code)
+                verifyCode(code)
+            }
+        }
+
+        override fun onVerificationFailed(exception: FirebaseException) {
+            Toast.makeText(this@OtpActivity,exception.message, Toast.LENGTH_SHORT).show()
+        }
+        override fun onCodeSent(verificationId: String, forceResendingToken: PhoneAuthProvider.ForceResendingToken) {
+            super.onCodeSent(verificationId, forceResendingToken)
+            storedVerificationId=verificationId
+        }
+    }
+
+    private fun sendCode(phoneNumber: String) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneNumber, 60, TimeUnit.SECONDS, this, callbacks)
+    }
+    private fun verifyCode(code:String){
+        val credential = PhoneAuthProvider.getCredential(storedVerificationId, code)
+        signInWithPhoneAuthCredential(credential)
+    }
+    private fun signInWithPhoneAuthCredential(phoneAuthCredential: PhoneAuthCredential) {
+        auth.signInWithCredential(phoneAuthCredential)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful){
+
+                    val intent=Intent(this,
+                        SocializingActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                }
+                else{
+                    Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/example/loveactuallymeandroidapp/ui/auth/SignUpActivity.kt
+++ app/src/main/java/com/example/loveactuallymeandroidapp/ui/auth/SignUpActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.loveactuallymeandroidapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 
@@ -23,18 +25,20 @@ class SignUpActivity : AppCompatActivity() {
 
         log_out_button.setOnClickListener {
            val phoneNumber= editTextPhone.text.toString()
-
             if (phoneNumber.isNotEmpty() && phoneNumber.length == 10) {
                 val firstDigit = phoneNumber.substring(0, 1).toInt()
                 if ((firstDigit == 6 || firstDigit == 7 || firstDigit == 8 || firstDigit == 9))
                 {
                     val  countryCode=countryCodePicker.selectedCountryCode.toString()
                     val mobileNumber= "+$countryCode$phoneNumber"
+
                     val preference=getSharedPreferences(resources.getString(R.string.app_name), Context.MODE_PRIVATE)
                     val editor=preference.edit()
                     editor.putString("mobilenumber",mobileNumber)
                     editor.apply()
-                    startActivity(Intent(this, OtpActivity::class.java))
+                    val i =Intent(this, OtpActivity::class.java)
+                    i.putExtra("phoneNo",mobileNumber)
+                    startActivity(i)
                     finish()
                 }
                 else{
