0c21c9b6e76156af910682393096a4d925bbf7e9
Preetish Niket <niketpreetish@gmail.com>
2020-09-21 23:24:05 +0530

    Adding Push Notification using FCM

---
+++ app/src/main/java/com/example/loveactuallymeandroidapp/Notification/MyFirebaseMessagingService.kt
@@ -0,0 +1,12 @@
+package com.example.loveactuallymeandroidapp.Notification
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyFirebaseMessagingService: Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+}
