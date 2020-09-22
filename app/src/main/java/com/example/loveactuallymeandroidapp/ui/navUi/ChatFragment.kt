1d2f93563c30d1a8139869829b33f0bc0f98e441
Preetish Niket <niketpreetish@gmail.com>
2020-09-22 22:13:30 +0530

    Adding code to check user Status

--- app/src/main/java/com/example/loveactuallymeandroidapp/ui/navUi/ChatFragment.kt
+++ app/src/main/java/com/example/loveactuallymeandroidapp/ui/navUi/ChatFragment.kt
@@ -26,7 +26,6 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.fragment_chat.*
 import kotlinx.android.synthetic.main.fragment_chat.view.*
 
 class ChatFragment : Fragment() {
