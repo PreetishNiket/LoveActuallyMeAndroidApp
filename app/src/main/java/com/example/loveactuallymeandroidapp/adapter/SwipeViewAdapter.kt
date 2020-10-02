42b6501b5ac593b2edb9247b8332fbef8bc3a20e
Preetish Niket <niketpreetish@gmail.com>
2020-10-02 23:19:16 +0530

    Working on Empty View

--- app/src/main/java/com/example/loveactuallymeandroidapp/adapter/SwipeViewAdapter.kt
+++ app/src/main/java/com/example/loveactuallymeandroidapp/adapter/SwipeViewAdapter.kt
@@ -1,19 +1,20 @@
 package com.example.loveactuallymeandroidapp.adapter
 
-import android.content.Context
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.example.loveactuallymeandroidapp.R
 import com.example.loveactuallymeandroidapp.dataClass.Users
 import com.huxq17.swipecardsview.BaseCardAdapter
-import com.squareup.picasso.Picasso
 
 class SwipeViewAdapter(private val modelList: ArrayList<Users>) : BaseCardAdapter<List<Users>>() {
-
-    override fun getCount(): Int =modelList.size
+    private var type:Int=0
+//    override fun getCount(): Int =modelList.size
 
     override fun getCardLayoutId(): Int {
+        if (type==1){
+            return R.layout.empty_layout
+        }
         return R.layout.item_card_view_profile
     }
 
@@ -23,5 +24,14 @@ class SwipeViewAdapter(private val modelList: ArrayList<Users>) : BaseCardAdapte
         val model =modelList[position]
         userName.text=model.getName()
 //        Picasso.get().load(model.userImage).into(userImage)
+    }
+
+    override fun getCount(): Int {
+        return if (modelList.size==0){
+            type=1
+            modelList.size
+        } else{
+            modelList.size
+        }
     }
 }
